14272778be97f56be22b84f2495ee3f2540be1b1
Mark Almeida-Cardy <mark@almeida-cardy.org>
2024-11-21 13:46:07 +0000

    Update openai.js

--- server/src/config/openai.js
+++ server/src/config/openai.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { logger } from '../utils/logger.js';
 
 // Hard-coded key for testing - REMOVE IN PRODUCTION
-const OPENAI_API_KEY = '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';
+const OPENAI_API_KEY = '';
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY
